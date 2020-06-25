2db6e12db9dfb4ede7935f76139b38c7bd46571a
RachanaMiryala <67002289+RachanaMiryala@users.noreply.github.com>
2020-06-25 12:51:20 +0530

    Update script.js

--- module 4/js/script.js
+++ module 4/js/script.js
@@ -1,7 +1,7 @@
 var names=["John","joe","Roy","Tina"];
 for(name in names) {
 if(name.charAt(0)=='j'||name.charAt[0]=='J')
-console.log("Goodbye" + name[i]);
+console.log("Goodbye" + name);
 else
-console.log("Hi" + name[i]);
+console.log("Hi" + name);
 }
